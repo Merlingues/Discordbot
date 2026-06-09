10aa9fc0d0b05b89f56ba1ced60ed5e9ab6f7f87
Merlingues <lecornu.merlin@proton.me>
2026-06-09 17:23:54 +0200

    modifications d'une commandes + ajouts de l'initialisation du nouveau cogs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 import os
 import asyncio
@@ -18,6 +19,8 @@ async def load_extensions():
         print("✅ Cog 'SalesFree' chargé avec succès !")
         await bot.load_extension("cogs.event")
         print("✅ Cog 'Event' chargé avec succès !")
+        await bot.load_extension("cogs.commandes")
+        print("✅ Cog 'Event' chargé avec succès !")
     except Exception as e:
         print(f"❌ Erreur lors du chargement du Cog : {e}")
 
@@ -27,12 +30,20 @@ async def main():
         await load_extensions()
         await bot.start(os.getenv('DISCORD_TOKEN'))
 
-@bot.command()
-async def refresh_sales(ctx):
-    cog = bot.get_cog('SalesFree')
+@bot.event
+async def on_ready():
+    await bot.tree.sync() # Obligatoire pour faire apparaître les commandes "/"
+    print(f"Connecté et commandes synchronisées pour {bot.user} !")
+
+@bot.tree.command(name="refresh_sales", description="Relance la vérification des promotions manuellement")
+async def refresh_sales(interaction: discord.Interaction):
+    cog = interaction.client.get_cog('SalesFree') 
+    
     if cog:
-        await ctx.send("Refresh en cours ...")
+        await interaction.response.send_message("Refresh en cours ...")
         await cog.check_sales()
+    else:
+        await interaction.response.send_message("Erreur : Le module 'SalesFree' est introuvable.", ephemeral=True)
 
 if __name__ == "__main__":
     asyncio.run(main())
